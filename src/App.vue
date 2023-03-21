4323a8f7ff72491a65246906457c24ded23d1a0e
honk456 <869934368@qq.com>
2023-03-21 17:01:05 +0800

    style: 完成了PC,移动端的响应式UI界面

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup lang="ts">
-import Dashboard from './views/Dashboard.vue'
+import Dashboard from "./views/Dashboard.vue";
 </script>
 
 <template>
   <Dashboard />
 </template>
 
-<style>
-
-</style>
+<style></style>
